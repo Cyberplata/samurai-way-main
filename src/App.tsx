f2e11f7fba7c847f10504476db31f317bece8232
PlatonKynin <kininplaton@gmail.com>
2024-01-15 20:46:48 +0300

    07.Уроки React JS (Что такое Компонента). Changed App component, add Technologies component and Header component. Add 4 files javascript 08 урок

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,34 @@ import './App.css';
 const App = () => {
     return (
         <div>
-            <div>one div</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
-            </div>
+            <Header/>
+            <Technologies/>
         </div>
     );
 }
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Messages</a>
+        </div>
+    );
+}
+
 
 export default App;
