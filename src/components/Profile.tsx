49e2e549bd53b73f3fbd282066360ed0534d3726
PlatonKynin <kininplaton@gmail.com>
2024-01-24 23:06:51 +0300

    14. Уроки React JS (css-модули, css-modules). Added my own styles via class to Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,16 +3,16 @@ import './Profile.module.css';
 import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img
                 src="https://media.istockphoto.com/id/1283376667/photo/autumn-lake-panorama.jpg?s=612x612&w=0&k=20&c=uAWmhFJTwBxCmiQwIwdMkWzjdfHgmicUqp5OauzegvM="
                 alt=""/>
         </div>
-        <div>
+        <div className={`${s.item} ${s.focus}`}>
             ava + description
         </div>
-        <div>
+        <div className={`${s.item} ${s.active}`}>
             My post
             <div>
                 New post
