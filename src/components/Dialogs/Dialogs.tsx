25b6ceaa2a05071f0bfb04d75f33ed520c172b06
PlatonKynin <kininplaton@gmail.com>
2024-01-28 18:26:30 +0300

    23. Уроки React JS (props для Диалогов) v1 - converted divs with people's names into props and placed them in the DialogsItem component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props: any) => {
+    let path = "/dialogs/" + props.id;
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+}
+
 const Dialogs = (props: any) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to="/dialogs/1">Dimych</NavLink>
-                </div>
-                <div className={s.dialog}>
+
+                <DialogItem name="Dimych" id="1"/>
+                <DialogItem name="Andrey" id="2"/>
+                <DialogItem name="Sveta" id="3"/>
+                <DialogItem name="Sasha" id="4"/>
+                <DialogItem name="Viktor" id="5"/>
+                <DialogItem name="Valery" id="6"/>
+
+                {/*<div className={s.dialog}>
                     <NavLink to="/dialogs/2">Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
@@ -23,12 +38,12 @@ const Dialogs = (props: any) => {
                 </div>
                 <div className={s.dialog}>
                     <NavLink to="/dialogs/6">Valery</NavLink>
-                </div>
+                </div>*/}
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>How is your it-kamasutra?</div>
-                <div className={s.message}>Yo</div>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your it-kamasutra?</div>
+                <div className={s.dialog}>Yo</div>
             </div>
         </div>
     );
