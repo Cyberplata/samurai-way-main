e8235c2c35a5715cdc6c8778ef5a95cfe5f06d08
PlatonKynin <kininplaton@gmail.com>
2024-01-28 16:09:30 +0300

    19. route, browser-router, маршрутизация + homework: added/created 3 components for routing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
-console.log(s)
-
-// let s = {
-//     'nav': 'Navbar_nav__3ou9Q',
-//     'item': 'Navbar_item__3qaF3',
-//     'active': 'Navbar_active__hyBfj'
-// }
-
-let c1 = "item";
-let c2 = "active";
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${s.active}`;
-// "item active"
-
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href="/dialogs">Messages</a>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <a href="/news">News</a>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <a href="/music">Music</a>
         </div>
         <div className={s.item}>
-            <a>Settings</a>
+            <a href="/settings">Settings</a>
         </div>
     </nav>
 }
